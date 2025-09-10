80ff026a6b765b4f122411225dd0958c2cd72147
mohith D <163638586+klu2300031066@users.noreply.github.com>
2025-09-10 10:31:32 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -15,5 +15,5 @@ export const getProducts = async (category = "") => {
 
 // Get product image URL
 export const getProductImageUrl = (imagePath) => {
-  return `http://localhost:8080/api/products/images/${imagePath}`;
+  return `http://ec2-54-167-6-243.compute-1.amazonaws.com:8080/api/products/images/${imagePath}`;
 };
